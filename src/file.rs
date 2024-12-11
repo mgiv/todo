b98cfba854b79438b81f4b0d04731dacd96a00c1
Matthew Given <143132572+bluescorpion35@users.noreply.github.com>
2024-12-11 19:06:19 +0000

    Add default contents to file
    Add the default contents (id = 0)
    to todo.toml if it doesn't exist or
    is empty

--- src/file.rs
+++ src/file.rs
@@ -44,6 +44,23 @@ pub fn open_file() -> Result<File, Box<dyn Error>> {
 
     path.push("todo.toml");
 
+    // Check if file is empty or doesn't exist and create it / add default data
+    let metadata = path.metadata()?;
+    let is_empty: bool = metadata.len() == 0;
+    println!("{}", is_empty);
+
+    if !path.exists() || is_empty  {
+        let mut file_creator = File::create(&path)?;
+        let mut default = toml::to_string(&TomlData {
+            config: Config {
+                id: 0,
+            },
+            todos: BTreeMap::new()
+        })?;
+        file_creator.write_all(default.as_bytes())?;
+
+    }
+
     Ok(OpenOptions::new()
         .read(true)
         .write(true)
